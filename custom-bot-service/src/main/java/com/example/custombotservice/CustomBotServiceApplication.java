ced09e77f73ff4b8efcac992b00b504809a812a5
Sakshi <sakshi.ahlawat@stackroute.in>
2023-09-04 14:41:56 +0530

    initial commit

---
+++ custom-bot-service/src/main/java/com/example/custombotservice/CustomBotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.custombotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomBotServiceApplication.class, args);
+	}
+
+}
