6f28f365d1679ec8c3e8bc4aa391ccad956c0055
rajnishpandey13words <rajnishpandey13words@gmail.com>
2023-09-05 18:04:45 +0530

    First commit

---
+++ custom-bot-service/src/main/java/com/example/custombotservice/config/OpenAIConfig.java
@@ -0,0 +1,14 @@
+package com.example.custombotservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class OpenAIConfig {
+	
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
+	}
+}
