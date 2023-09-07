a4dd4522f418119c0baa8896a7053f666cdf55ac
navneetchapke <navneetchapke@gmail.com>
2023-09-07 15:53:19 +0530

    service implemented

--- notification-service/src/main/java/com/example/notificationservice/model/NotificationModel.java
+++ notification-service/src/main/java/com/example/notificationservice/model/NotificationModel.java
@@ -1,23 +1,52 @@
 package com.example.notificationservice.model;
 
+import java.math.BigInteger;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Document(collection = "notificationVacancyCreated")
 public class NotificationModel {
 	
 	@Id
-	long userID;
-	String userName;
-	String email;
-	String sub;
-	String notificationMsg;
+	private BigInteger userId;
+	private String userName;
+	private String email;
+//	String sub;
+	private String notificationMsgType; // for vacancy created notification notificationMsgType = "vacancyCreated"
+	private String spotId;
+	public BigInteger getUserId() {
+		return userId;
+	}
+	public void setUserId(BigInteger userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNotificationMsgType() {
+		return notificationMsgType;
+	}
+	public void setNotificationMsgType(String notificationMsgType) {
+		this.notificationMsgType = notificationMsgType;
+	}
+	public String getSpotId() {
+		return spotId;
+	}
+	public void setSpotId(String spotId) {
+		this.spotId = spotId;
+	}
+	
+
 	
 }
