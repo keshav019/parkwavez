e52ac8470459058f886557be11b9c6a1e00c20b8
navneetchapke <navneetchapke@gmail.com>
2023-09-11 08:12:52 +0530

    Exception Handling implemented

--- notification-service/src/main/java/com/example/notificationservice/consumer/KafkaMessageListener.java
+++ notification-service/src/main/java/com/example/notificationservice/consumer/KafkaMessageListener.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.example.notificationservice.service.NotificationService;
+import com.example.notificationservice.exception.MessageProcessingException;
 import com.kafkaProducer.UserDTO;
 import com.kafkaProducer.UserSpotIdDTO;
 
@@ -20,16 +21,26 @@ public class KafkaMessageListener {
     
     @KafkaListener(topics = "topicNew62", groupId = "group14")
     public void consume(UserDTO user) {
+    	try {
         log.info("consumer consume {}", user);
         String emailBody = "Dear " + user.getUserName() + " Your Application Created with User Id " + user.getUserId() + " .";
         notificationService.sendEmail(user.getEmail(), "Account Successfully Created", emailBody);
+    	} catch (Exception ex) {
+    		log.error("Error processing Kafka message for user: {}", user, ex);
+            throw new MessageProcessingException("Error processing Kafka message for user: " + user, ex);
+    	}
     }
     
     @KafkaListener(topics = "vacancyCreated2", groupId = "group14")
     public void consume2(UserSpotIdDTO user2) {
+    	try {
         log.info("consumer consume {}", user2);        
         String emailBody = "Dear " + user2.getUserName() + " spot ID " + user2.getSpotId() + " is now available for booking.";
         notificationService.sendEmail(user2.getUserEmail(), "Your Subscribed Spot is now available for Booking", emailBody);
+    	} catch (Exception ex) {
+    		log.error("Error processing Kafka message for user: {}", user2, ex);
+            throw new MessageProcessingException("Error processing Kafka message for user: " + user2, ex);
+    	}
     }
     
 }
