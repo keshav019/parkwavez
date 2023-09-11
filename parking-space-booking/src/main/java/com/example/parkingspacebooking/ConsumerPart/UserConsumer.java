faf28cb9bc4f8dfa5a1ffff355f45a8acc1ec3f0
gauravraj2626 <gauravraj2626@gmail.com>
2023-09-11 13:55:56 +0530

    Initial commit

---
+++ parking-space-booking/src/main/java/com/example/parkingspacebooking/ConsumerPart/UserConsumer.java
@@ -0,0 +1,29 @@
+package com.example.parkingspacebooking.ConsumerPart;
+
+/*
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserConsumer {
+	
+	 
+
+	private static final Logger LOGGER =LoggerFactory.getLogger(UserConsumer.class);
+	@KafkaListener(topics="${spring.kafka.topic.name}",groupId="${spring.kafka.consumer.group-id}")
+	public void consumer(String message) {
+		
+		LOGGER.info(String.format("UserEvent received in Booking service => %s",message));
+		
+		//save the order event into the database
+		
+		
+		
+	}
+	}
+	
+*/
