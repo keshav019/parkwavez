3fa57dffd71d92ebd46c46b8a0da5d19716cf11f
navneetchapke <navneetchapke@gmail.com>
2023-09-14 15:05:37 +0530

    Fixed Error

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentComponent } from './payment/payment.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,16 +22,16 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
-<<<<<<< HEAD
+
 import {MatDatepickerModule} from '@angular/material/datepicker'; 
 import { MAT_DATE_FORMATS, DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatNativeDateModule } from '@angular/material/core';
 
 
-import { FormsModule } from '@angular/forms';
-=======
+
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
->>>>>>> 2f2bfc592a31931baf8ebd7b00693fe7f76bbc53
+
 import { MatInputModule } from '@angular/material/input';
 import { CarouselModule } from './carousel/carousel.module';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -72,11 +72,10 @@ import { RegistrationComponent } from './registration/registration.component';
     MatSelectModule,
     MatFormFieldModule,
     MatTableModule,
-<<<<<<< HEAD
     MatDatepickerModule,
-    MatNativeDateModule
-=======
->>>>>>> 2f2bfc592a31931baf8ebd7b00693fe7f76bbc53
+    MatNativeDateModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
