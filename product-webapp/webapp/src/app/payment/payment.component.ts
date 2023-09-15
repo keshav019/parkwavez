7d5799f24b7cafe50bc462c7dc27171885fa5e55
harshitgupta7755 <harshitgupta7755@gmail.com>
2023-09-15 18:12:44 +0530

    15 sept

--- product-webapp/webapp/src/app/payment/payment.component.ts
+++ product-webapp/webapp/src/app/payment/payment.component.ts
@@ -1,99 +1,86 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-// const Razorpay = require('razorpay');
-const Razorpay: any = {}; //Added this line and commented line 3 and 15
-//declare var Razorpay: any;
+declare var Razorpay: any;
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
+
+
 export class PaymentComponent {
+
   amount!: number;
-  rzp: any;
-  
-  paymentStart() {
-    console.log('Payment started..');
-    if (!this.amount || this.amount <= 0) {
-      alert('Please enter a valid amount');
-      return;
-    }
-    // Send a request to your server to create an order
-    fetch('http://localhost:8087/user/Create_Order', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+  backendUrl = 'http://localhost:8087/user/Create_Order';
+  constructor(private http: HttpClient) {}
+
+   paymentStart() {
+
+    this.http.post(this.backendUrl,{"amount":this.amount}).subscribe(
+      (response) => {
+          console.log(response);
+          this.openPaymentForm(response);
       },
-      body: JSON.stringify({ "amount": `${this.amount}` }),
-    })
-      .then((response) => response.json())
+      error => {
+        console.log(error);
+      }
+    );
 
-      .then((response) => {
-        console.log(response.status);
-        if (response.status === 'created') {
-          const options = {
-            key: 'rzp_test_aPJeknHS2DZpsD',
-            amount: response.amount,
-            currency: 'INR',
-            name: 'Parkwavez',
-            description: 'Payment',
-            image: '',
-            order_id: response.id,
-            handler: (response: {
-              razorpay_payment_id: any;
-              razorpay_order_id: any;
-              razorpay_signature: any;
-            }) => {
-              console.log(response.razorpay_payment_id);
-              console.log(response.razorpay_order_id);
-              console.log(response.razorpay_signature);
-              console.log('Payment successful!!');
-              alert('Payment done successfully');
-            },
-            prefill: {
-              name: '',
-              email: '',
-              contact: '',
-            },
-            notes: {
-              address: 'PARKWAVEZ',
-            },
-            theme: {
-              color: '#3399cc',
-            },
-          };
-          console.log('code run till here');
-          const rzp = new Razorpay();
-          rzp.on(
-            'payment.failed',
-            function (response: {
-              error: {
-                code: any;
-                description: any;
-                source: any;
-                step: any;
-                reason: any;
-                metadata: { order_id: any; payment_id: any };
-              };
-            }) {
-              console.log(response.error.code);
-              console.log(response.error.description);
-              console.log(response.error.source);
-              console.log(response.error.step);
-              console.log(response.error.reason);
-              console.log(response.error.metadata.order_id);
-              console.log(response.error.metadata.payment_id);
-              alert('Payment Failed');
-            }
-          );
+   }
+   openPaymentForm(response: any) {
+    var options = {
+      order_id: response.id,
+      key:'rzp_test_aPJeknHS2DZpsD',
+      amount: response.amount,
+      currency: 'INR',
+      name:'PARKWAVEZ',
+      description:'Payment gateway',
+      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE3Mcuw4UiAE-DaAbiEgy8I-KUMlljWwbpuf_XbzTJeG1GWTynmagoqsFLQhaxKxEsFwA&usqp=CAU',
 
-          rzp.open();
+      handler:(response:any) => {
+        if(response!=null&& response.razorpay_payment_id!= null){
+        this.processResponse(response);
+        this.updatePaymentOnServer(response.razorpay_payment_id,response.razorpay_order_id,"Paid");
         }
-      })
-      .catch((error) => {
-        console.error(error);
-        alert('Something went wrong !!');
-      });
-  }
+        else{
+          alert("Payment failed..");
+        }
+      },
+      prefill:{
+        name:'',
+        mobile:'',
+        email:'',
+      },
+      notes:{
+        address:'Parking Booking'
+      },
+      theme:{
+        color:'#007bff'
+      }
+    };
+    var razorOb =new Razorpay(options);
+    razorOb.open();
+   }
+   processResponse(resp:any){
+    console.log(resp);
+   }
+
+   updatePaymentOnServer(payment_id: any, order_id: any, status: string){
+
+    this.http.post('http://localhost:8087/user/update_order',{"payment_id":payment_id,
+    "order_id":order_id,
+    "status":status}).subscribe(
+      (response) => {
+      console.log(response);
+      alert("PAID SUCCESSFULLY");
+    },
+    error => {
+      alert("Your payment is succesful but not updated to our server")
+    }
+   );
+
+   }
+
 }
