e392698b96f2cec178e4b4e340e153baca10b8fc
navneetchapke <navneetchapke@gmail.com>
2023-09-19 19:24:09 +0530

    Commit

--- product-webapp/webapp/src/app/components/area-card/area-card.component.ts
+++ product-webapp/webapp/src/app/components/area-card/area-card.component.ts
@@ -9,127 +9,15 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./area-card.component.css'],
 })
 export class AreaCardComponent implements OnInit {
-  products = [
-    {
-      name: 'Product 1',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-        rating: 4.5,
-        category: 'Electronics',
-        brand: 'Brand X',  
-    },
-    {
-      name: 'Product 2',
-      description: 'Description for Product 2',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 3',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 4',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 5',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 6',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 7',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-    {
-      name: 'Product 8',
-      description: 'Description for Product 1',
-      imageUrl:
-        'https://media.istockphoto.com/id/1157655660/photo/generic-red-suv-on-a-white-background-side-view.jpg?s=1024x1024&w=is&k=20&c=L99mV4LAN-MMUYe8kkrqaRhUHCqTLl4r_HIhqdNpHTg=',
-        price: 499.99,
-      rating: 4.5,
-      category: 'Electronics',
-      brand: 'Brand X',
-    },
-  ];
-
-  @ViewChild('carousel') carousel!: ElementRef;
-
-
-  selectedProduct: any;
+  
 
 
 
-
-   constructor(public dialog: MatDialog) {}
+   constructor() {}
   //constructor() {}
 
 
   ngOnInit(): void {}
-  scrollLeft(event: Event) {
-    event.stopPropagation();
-    this.carousel.nativeElement.scrollLeft -=
-      this.carousel.nativeElement.offsetWidth;
-  }
-  scrollRight(event: Event) {
-    event.stopPropagation();
-    this.carousel.nativeElement.scrollLeft +=
-      this.carousel.nativeElement.offsetWidth;
-  }
-
-  showProductDetails(product: any) {
-    this.selectedProduct = product;
-
-    const dialogRef = this.dialog.open(ProductDetailsComponent, {
-      disableClose: false,
-      width: '1000px',
-      height: '500px',
-      data: this.selectedProduct,
-      hasBackdrop: true,
-    });
-  }
-
-  closeProductDetails() {
-    this.selectedProduct = null;
-  }
+  
 
 }
